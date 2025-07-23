9251a59c1961338aaeb839f2af2f95654fd007cd
dalecb13 <dalecb13@gmail.com>
2025-07-23 10:03:56 -0500

    added active navigation style

--- components/navigation-bar.tsx
+++ components/navigation-bar.tsx
@@ -1,15 +1,30 @@
+'use client';
+
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
 import { NavigationMenu } from "radix-ui";
 
 export default function NavigationBar () {
+  const pathname = usePathname();
+
   return (
     <NavigationMenu.Root className="flex gap-5">
-      {/* <NavigationMenu.Link asChild active={pathname.startsWith("/purveyors")}> */}
-      <NavigationMenu.Link asChild>
+      <NavigationMenu.Link
+        asChild
+        active={pathname.startsWith("/purveyors")}
+        className={`NavigationMenuLink hover:text-blue-500
+          ${pathname.startsWith("/purveyors") ? "underline-offset-1" : ""}
+        `}
+      >
         <Link href="/purveyors">Purveyors</Link>
       </NavigationMenu.Link>
-      {/* <NavigationMenu.Link asChild active={pathname === "/documents"}> */}
-      <NavigationMenu.Link asChild>
+      <NavigationMenu.Link
+        asChild
+        active={pathname.startsWith("/offerings")}
+        className={`NavigationMenuLink hover:text-blue-500
+          ${pathname.startsWith("/offerings") ? "underline-offset-1" : ""}
+        `}
+      >
         <Link href="/offerings">Offerings</Link>
       </NavigationMenu.Link>
     </NavigationMenu.Root>
